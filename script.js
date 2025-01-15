f7a377fdcd6d0a47c3c2eddb1d7cfd11b6b3359c
RihannaP <114884466+RihannaP@users.noreply.github.com>
2025-01-15 20:18:18 +0000

    Implement search and counter function

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
 function setup() {
   const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
+  let searchTerm = "";  // Initial searchTerm state
+  const states = {allEpisodes, searchTerm};
+  render(states); //Initial render
+  createSearchTerm(states);  //Setup search functionality
 }
 
+function render(stateList) { 
+    //Filter episodes based on the searchTerm (case-insensitive)
+    const filteredEpisode = stateList.allEpisodes.filter((episode) => 
+        (episode.name.toLowerCase().includes(stateList.searchTerm.toLowerCase())
+        || 
+        episode.summary.toLowerCase().includes(stateList.searchTerm.toLowerCase()))
+    );
+    makePageForEpisodes(filteredEpisode);
+    episodeCounter(filteredEpisode.length,stateList.allEpisodes.length);
+}
 
-//level 100
+function createSearchTerm(stateList) {
+    const searchBox = document.getElementById("search");
+    searchBox.addEventListener("input", (event) => {  // Add an event listener to check user input
+        stateList.searchTerm = event.target.value;  //Update the searchTerm in state
+        render(stateList);    //Re-render with the updated state
+    })
+}
+// Function to update how many episodes are displayed
+function episodeCounter(filteredCount, totalCount) { 
+  const countElement = document.getElementById("episodeCount");
+  countElement.textContent = `Displaying ${filteredCount}/${totalCount} episodes.`;
+}
+
 function makePageForEpisodes(episodesList) {
   // Get the root element from the DOM where the episodes will be displayed
   const rootElem = document.getElementById("root");
+  rootElem.textContent = ``;  //clearing textContent
 
   const episodesContainer = document.createElement("div");
   episodesContainer.className = "episodes-container";
