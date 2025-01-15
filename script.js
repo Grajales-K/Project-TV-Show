8dc7d855a15a28325ea64da328a6aef45048108e
RihannaP <114884466+RihannaP@users.noreply.github.com>
2025-01-15 20:38:25 +0000

    implement select function

--- script.js
+++ script.js
@@ -15,6 +15,33 @@ function render(stateList) {
     );
     makePageForEpisodes(filteredEpisode);
     episodeCounter(filteredEpisode.length,stateList.allEpisodes.length);
+    selectEpisodes(stateList.allEpisodes)
+}
+//Make a select list and link to the episode
+function selectEpisodes(episodeList){
+    const selectList = document.getElementById("select")
+    for(episode of episodeList){ 
+        const optionList = document.createElement("option");
+        optionList.value = episode.id;
+        const episodeSeason = String(episode.season).padStart(2, "0");
+        const episodeNumber= String(episode.number).padStart(2, "0");
+        optionList.textContent = `S${episodeSeason}E${episodeNumber} - ${episode.name} `;
+        selectList.append(optionList)
+    }
+    selectList.addEventListener("change", (event) => {
+       const selectedId = event.target.value;
+
+       if(selectedId === "allEpisodes"){ 
+        makePageForEpisodes(episodeList);
+        episodeCounter(episodeList.length,episodeList.length);
+       }
+       else {
+        const selectEpisode = Array(episodeList.find((episode) => episode.id == selectedId));
+        makePageForEpisodes(selectEpisode);
+        episodeCounter(1,episodeList.length);
+       }; 
+    })
+    
 }
 
 function createSearchTerm(stateList) {
